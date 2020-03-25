8b246425768b7ac98bee78543847da7e6c2d8728
DunkRing <Chris31@live.dk>
2020-03-25 19:44:56 +0100

    Project added

---
+++ airport-template-master/src/main/java/dk/cphbusiness/airport/template/Program.java
@@ -0,0 +1,34 @@
+package dk.cphbusiness.airport.template;
+
+import dk.cphbusiness.algorithm.examples.queues.NotPrioritisingPassengerArrayQueue;
+import dk.cphbusiness.algorithm.examples.queues.PriorityQueue;
+import dk.cphbusiness.algorithm.examples.queues.PriorityQueueUsingHeap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+  private static List<Plane> planes = new ArrayList<>();
+  private static PassengerProducer producer;
+  private static PassengerConsumer consumer;
+  private static PriorityQueueUsingHeap<Passenger> queue;
+  private static Clock clock;
+  
+  private static void setup() {
+    for (int hour = 7; hour <= 22; hour++) {
+      planes.add(new Plane(new Time(hour, 00, 00)));
+      }
+    queue = new PriorityQueueUsingHeap<Passenger>(10000);
+    producer = new PassengerProducer(planes, queue);
+    consumer = new PassengerConsumer(planes, queue);
+    clock = new Clock(producer, consumer, new Time(05, 00, 00));
+    }
+  
+  public static void main(String[] args) {
+    setup();
+    System.out.println("Hello Airport");
+    new Thread(clock).start();
+    
+    }
+  
+  }
---
+++ airport-template-master/src/main/java/dk/cphbusiness/algorithm/examples/queues/PriorityQueueUsingHeap.java
@@ -0,0 +1,103 @@
+package dk.cphbusiness.algorithm.examples.queues;
+
+import dk.cphbusiness.airport.template.Passenger;
+
+public class PriorityQueueUsingHeap<T extends Comparable<T>>  {
+
+    T[] arr;
+    int N = 0;
+
+
+    public PriorityQueueUsingHeap(int maxN){
+        // arr = (T []) new Comparable[2];
+        arr = (T[]) new Comparable[maxN + 1];
+
+    }
+
+
+    //helper methods
+    public String toString(){
+        StringBuffer sb = new StringBuffer();
+        for(int i = 1; i <= N; i ++)
+            sb.append(arr[i].toString()+" ");
+        return sb.toString();
+    }
+
+
+    public boolean isEmpty(){
+        return N == 0;
+    }
+    private void resize(int capacity){
+        T[] copy = (T []) new Comparable[capacity];
+        for(int i = 1; i <= N; i ++ )
+            copy[i] = arr[i];
+        arr = copy;
+    }
+
+    private void swim(int k){
+        while(k > 1 && less(k/2, k)){
+            exch(k/2,k);
+            k = k/2;
+        }
+    }
+
+    private void sink(int k){
+        while (2*k < N){
+            int j = 2 * k;
+            if(j < N && less(j, j +1)) j = j + 1;
+            if(less(j, k)) break;
+            exch(k,j);
+            k = j;
+        }
+    }
+
+    private boolean less(int i, int j){
+        if (arr[i].compareTo(arr[j]) < 0)
+            return true;
+        return false;
+    }
+
+    private void exch(int i, int j){
+        T temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+
+
+    public void enqueue(T item) {
+        if (N == arr.length - 1)
+            resize(2*N + 1);
+        arr[++N] = item;
+        swim(N);
+    }
+
+
+    public T dequeue() {
+        if (isEmpty()) return null;
+        T t= arr[1];
+        exch(1,N--);
+        arr[N+1] = null;
+        sink(1);
+
+        //resize this array
+        if (N == (arr.length -1)/4) resize((arr.length-1) / 2 + 1);
+        return t;
+    }
+
+
+    public T peek() {
+        if (N== 0) {
+            return null;
+        }
+        else {
+            return arr[0];
+        }
+    }
+
+
+    public int size() {
+        return N;
+    }
+
+}
